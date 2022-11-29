f7af34bf6a8cc606a223534469c5066f6930aa2e
MrThong24 <thong.buichi@ncc.asia>
2022-11-29 21:06:36 +0700

    up load code header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -9,6 +9,8 @@ import { logout } from "../actions/userActions";
 import Authorization from "../hook/Authorization";
 import { RolesEnums } from "../routes/ProtectedRoute";
 import history from "../utils/history";
+import "../styles/header.css";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   const dispatch = useDispatch();
@@ -19,58 +21,100 @@ const Header = () => {
   const logoutHandler = () => {
     dispatch(logout());
   };
-  const handerChangeAdmin = () => {
+  const handelChangeAdmin = () => {
     history.push("/admin");
   };
 
   return (
-    <div>
-      <header>
-        <Navbar bg="primary" variant="dark" expand="lg" collapseOnSelect>
-          <Container>
-            <LinkContainer to="/">
-              <Navbar.Brand>React E-Commerce</Navbar.Brand>
-            </LinkContainer>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="ml-auto">
-                <LinkContainer to="/cart">
-                  <Nav.Link>
-                    <i className="fa fa-shopping-cart mr-2"></i>
-                    Cart
-                  </Nav.Link>
-                </LinkContainer>
-                {userInfo ? (
-                  <NavDropdown title={userInfo.name} id="username">
-                    <LinkContainer to="/profile">
-                      <NavDropdown.Item>Profile</NavDropdown.Item>
-                    </LinkContainer>
-                    <Authorization
-                      roles={[RolesEnums.get("ADMIN")]}
-                      type="ifAnyGranted"
-                    >
-                      <NavDropdown.Item onClick={handerChangeAdmin}>
-                        Admin
-                      </NavDropdown.Item>
-                    </Authorization>
-                    <NavDropdown.Item onClick={logoutHandler}>
-                      Logout
-                    </NavDropdown.Item>
-                  </NavDropdown>
-                ) : (
-                  <LinkContainer to="/login">
-                    <Nav.Link>
-                      <i className="fa fa-user mr-2"></i>
-                      Sign In
-                    </Nav.Link>
-                  </LinkContainer>
-                )}
-              </Nav>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-      </header>
-    </div>
+    <Navbar className="header" variant="dark" expand="lg" collapseOnSelect>
+      <LinkContainer to="/">
+        <Navbar.Brand className="header-top">React E-Commerce</Navbar.Brand>
+      </LinkContainer>
+      <ul className="header-nav">
+        <li className="header-nav-items">
+          <NavLink className="item-links" to="/home">
+            Home
+            <i className="bx bx-chevron-down"></i>
+          </NavLink>
+          <ul className="nav-child">
+            <li className="nav-child-items">home 1</li>
+            <li className="nav-child-items">home 2</li>
+          </ul>
+        </li>
+        <li className="header-nav-items">
+          <NavLink className="item-links" to="/shop">
+            shop
+            <i className="bx bx-chevron-down"></i>
+          </NavLink>
+          <ul className="nav-child">
+            <li className="nav-child-items">home 1</li>
+            <li className="nav-child-items">home 2</li>
+          </ul>
+        </li>
+        <li className="header-nav-items">
+          <NavLink className="item-links" to="/products">
+            products
+            <i className="bx bx-chevron-down"></i>
+          </NavLink>
+          <ul className="nav-child">
+            <li className="nav-child-items">home 1</li>
+            <li className="nav-child-items">home 2</li>
+          </ul>
+        </li>
+        <li className="header-nav-items">
+          <NavLink className="item-links" to="/blog">
+            blog
+          </NavLink>
+        </li>
+        <li className="header-nav-items">
+          <NavLink className="item-links" to="/about">
+            about us
+          </NavLink>
+        </li>
+        <li className="header-nav-items">
+          <NavLink className="item-links" to="/contact">
+            contact us
+          </NavLink>
+        </li>
+      </ul>
+      <div className="header-bottom">
+        <div className="header-search">
+          <i className="bx bx-search"></i>
+        </div>
+        <div className="search-box">
+          <input type="text" placeholder="Search in here" />
+        </div>
+        <div className="header-cart">
+          <NavLink className="cart" to="/cart">
+            <i className="bx bx-cart"></i>
+          </NavLink>
+        </div>
+        <div className="header-login">
+          {userInfo ? (
+            <NavDropdown title={userInfo.name} id="username">
+              <LinkContainer to="/profile">
+                <NavDropdown.Item>Profile</NavDropdown.Item>
+              </LinkContainer>
+              <Authorization
+                roles={[RolesEnums.get("ADMIN")]}
+                type="ifAnyGranted"
+              >
+                <NavDropdown.Item onClick={handelChangeAdmin}>
+                  Admin
+                </NavDropdown.Item>
+              </Authorization>
+              <NavDropdown.Item onClick={logoutHandler}>
+                Logout
+              </NavDropdown.Item>
+            </NavDropdown>
+          ) : (
+            <NavLink className="login" to="/login">
+              <i className="bx bx-user"></i>
+            </NavLink>
+          )}
+        </div>
+      </div>
+    </Navbar>
   );
 };
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,20 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
-import store from './store'
-import './bootstrap.min.css'
-import './index.css'
-import App from './App'
-import * as serviceWorker from './serviceWorker'
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+import "./bootstrap.min.css";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
+  <BrowserRouter>
     <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById('root')
-)
+      <App />
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
+serviceWorker.unregister();
